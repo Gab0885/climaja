e76bb36eac1645ed254725b02f16e4ae4827e4f6
Gabriel Dias <gabrieldg0885@gmail.com>
2025-01-10 15:54:45 -0300

    added error class and middleware

---
+++ backend/src/middlewares/error-handler.ts
@@ -0,0 +1,11 @@
+import { ErrorRequestHandler } from "express";
+import { HttpError } from "../../errors/HttpError";
+
+export const errorHandlerMiddleware: ErrorRequestHandler = (error, req, res, next) => {
+    if (error instanceof HttpError) {
+        res.status(error.status).json({ message: error.message })
+    } else if (error instanceof Error) {
+        res.status(500).json({ message: error.message })
+    }
+    res.status(500).json({ message: "Erro interno desconhecido no servidor" })
+}
