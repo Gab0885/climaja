6157e9464f4a0f05658f4a727e271d05eb705107
Gabriel Dias <gabrieldg0885@gmail.com>
2025-01-15 17:42:30 -0300

    added theme switcher to index

---
+++ frontend/public/src/scripts/typescript/handlers/themeHandler.ts
@@ -0,0 +1,18 @@
+// Logica para troca de tema
+const themeSwitcher = document.querySelector(".fa-solid.fa-circle-half-stroke")
+if (themeSwitcher) {
+  themeSwitcher.addEventListener("click", () => {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute("data-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+
+    html.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  });
+}
+
+// Carrega o tema salvo ao iniciar a página
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme") || "dark";
+  document.documentElement.setAttribute("data-theme", savedTheme);
+});
